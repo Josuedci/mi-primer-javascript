3f03f9626e4bed662f542dcdba916e38a6931516
josuejdci <josuedavidchavez17@gmail.com>
2023-08-09 21:05:27 -0600

    primerospasosjs

---
+++ intro.js
@@ -0,0 +1,14 @@
+let variable =3
+let string = "seba"
+let x = "3"
+let boolean = "3" + 5
+//          0    1       2     3  4
+let array =[2,"josue", false, `p`, 3.5]
+
+console.log(variable)
+console.log(string)
+console.log(x)
+console.log (boolean)
+console.log(array.length)
+console.log
+
